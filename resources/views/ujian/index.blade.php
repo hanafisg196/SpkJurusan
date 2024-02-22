987950d90fa51a245479f123d6e538263552f907
Syamra <afdhaluzzikrisyamra1@gmail.com>
2024-02-22 22:57:47 +0700

    tampilan ujiann

--- resources/views/ujian/index.blade.php
+++ resources/views/ujian/index.blade.php
@@ -1,6 +1,30 @@
 @extends('tampilan.main')
 @section('content')
 
+<div class="col-lg-12">
+    <!-- Contextual classes table starts -->
+        <div class="card">
+            <div class="card-header">
+                <h2 style="text-align: center">Ujian Penentuan Jurusan</h2>
+            </div>
+            <div class="card-block" style="text-align: center">
+                        <p>Selamat datang, calon siswa SMK! Saat ini, Anda memasuki fase penentuan jurusan, momen krusial dalam perjalanan pendidikan Anda.
+                            Ujian ini adalah langkah awal untuk menemukan keahlian dan minat yang akan membimbing Anda meraih sukses di dunia kerja.
+                        </p>
+                        <p>
+                            Ingatlah, setiap pertanyaan didesain untuk menggali potensi unik dan kecakapan Anda di berbagai bidang.
+                            Gunakan kesempatan ini untuk mengeksplorasi minat Anda dan menemukan jurusan yang sesuai dengan impian karir masa depan Anda.
+                        </p>
+                        <p>
+                            Sukses mengikuti ujian penentuan jurusan SMK! Gunakan dengan bijak setiap pertanyaan sebagai langkah menuju masa depan sukses dan memuaskan.
+                            Kami yakin Anda akan menemukan jurusan yang menciptakan landasan kokoh untuk kesuksesan karir Anda di SMK. Selamat menemukan potensi terbaik Anda!</p>
+                        </p>
+                            <a href="/ujian/create" class="btn btn-primary mb-3 ">Mulai Ujain</a>
+
+            </div>
+
+        </div>
+    </div>
 
 
 @endsection
